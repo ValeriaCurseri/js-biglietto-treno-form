ea33f2e07a8cd93d86207b6eb094729f27e92f11
Valeria Curseri <curserivaleria@gmail.com>
2020-07-16 19:38:38 +0200

    Versione con una sola funzione

--- js/script.js
+++ js/script.js
@@ -9,38 +9,23 @@
 // Categoria selezionata dall'utente
 // Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
 
+
 // ***** funzioni ***** //
 
 // creo una funzione attiva al click sul bottone Genera
 
-document.getElementById('genera').addEventListener('click', function(){     // 1. compaia #recap
-    var button = document.getElementById('genera');
+document.getElementById('genera').addEventListener('click', function(){
+    var button = document.getElementById('genera');                         // 1. compaia #recap
     var recap = document.getElementById('recap');
     recap.classList.add('display-block');
     recap.classList.remove('display-none');
-}
-);
-
-document.getElementById('genera').addEventListener('click', function(){     // 2. si implementi il campo nome-Cognome
-    var nomeCognome = document.getElementById('nome-cognome').value;
+    var nomeCognome = document.getElementById('nome-cognome').value;        // 2. si implementi il campo nome-Cognome
     document.getElementById('recap-nome-cognome').innerHTML = nomeCognome;
-}
-);
-
-document.getElementById('genera').addEventListener('click', function(){     // 3. si generi il numero casuale della carrozza (da 1 a 9)
-    var carrozza = Math.floor(Math.random() * 10);
+    var carrozza = Math.floor(Math.random() * 10);                          // 3. si generi il numero casuale della carrozza (da 1 a 9)
     document.getElementById('recap-carrozza').innerHTML = carrozza;
-}
-);
-
-document.getElementById('genera').addEventListener('click', function(){     // 4. si generi il codice del treno (da 90000 a 99999)
-    var codice = Math.floor(Math.random() * (99999 - 90000 + 1)) + 90000;
+    var codice = Math.floor(Math.random() * (99999 - 90000 + 1)) + 90000;   // 4. si generi il codice del treno (da 90000 a 99999)
     document.getElementById('recap-codice').innerHTML = codice;
-}
-);
-
-document.getElementById('genera').addEventListener('click', function(){     // 5. si implementi il prezzo del biglietto
-    var chilometri = document.getElementById('chilometri').value;
+    var chilometri = document.getElementById('chilometri').value;           // 5. si implementi il prezzo del biglietto
     var eta = document.getElementById('età').value;
     var prezzo = 0.21 * chilometri;
     prezzo = prezzo.toFixed(2);
