f89718135cb13b4debba5615c4b9764fc08abe43
Valeria Curseri <curserivaleria@gmail.com>
2020-07-16 18:59:32 +0200

    Funzioni per dati di input sistemate (senza controlli)

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
 
 // ***** funzioni ***** //
 
-// creo una funzione per fare in modo che al click sul bottone Genera
+// creo una serie di funzioni attive al click sul bottone Genera
 
 document.getElementById('genera').addEventListener('click', function(){     // 1. compaia #recap
     var button = document.getElementById('genera');
@@ -29,7 +29,7 @@ document.getElementById('genera').addEventListener('click', function(){     // 2
 );
 
 document.getElementById('genera').addEventListener('click', function(){     // 3. si generi il numero casuale della carrozza (da 1 a 9)
-    var carrozza = Math.floor((Math.random() * 10) + 0);
+    var carrozza = Math.floor(Math.random() * 10);
     document.getElementById('recap-carrozza').innerHTML = carrozza;
 }
 );
@@ -44,21 +44,28 @@ document.getElementById('genera').addEventListener('click', function(){     // 5
     var chilometri = document.getElementById('chilometri').value;
     var eta = document.getElementById('età').value;
     var prezzo = 0.21 * chilometri;
+    prezzo = prezzo.toFixed(2);
     var offerta = 'Biglietto standard';
     if (eta == 'minorenne'){
         prezzo *= 0.8;
-        document.getElementById('recap-costo').innerHTML = prezzo;
+        prezzo = prezzo.toFixed(2);
+        document.getElementById('recap-costo').innerHTML = '€ ' + prezzo;
         offerta = 'Biglietto scontato per minori di 18 anni';
         document.getElementById('recap-offerta').innerHTML = offerta;
     } else if (eta == 'over'){
         prezzo *= 0.6;
-        document.getElementById('recap-costo').innerHTML = prezzo;
+        prezzo = prezzo.toFixed(2);
+        document.getElementById('recap-costo').innerHTML = '€ ' + prezzo;
         offerta = 'Biglietto scontato per over 65';
+        document.getElementById('recap-offerta').innerHTML = offerta;
+    } else {
+        document.getElementById('recap-costo').innerHTML = '€ ' + prezzo;
         document.getElementById('recap-offerta').innerHTML = offerta;
     }
 }
 );
 
+// creo una funzione che pulisca i dati al click sul bottone Annulla
 
 
 
